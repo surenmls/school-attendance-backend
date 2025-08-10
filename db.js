0f46e95ed001be781db9b5d7214bd183ef012eeb
surenmls <46064186+surenmls@users.noreply.github.com>
2025-08-10 22:31:32 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,6 @@
 // backend/db.js
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'school_attendance',
-//   password: 'admin',
-//   port: 5432,
-// });
-
-// const pool = new Pool({
-//   user: 'school_attendance_db_user',
-//   host: 'dpg-d2cc918gjchc73806t6g-a',
-//   database: 'school_attendance_db',
-//   password: 'mn187EImcPDxnCZF4uomkONE97oi7MDo',
-//   port: 5432,
-// });
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false } /// required for Render
